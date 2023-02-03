489ffed2ced043662b535639872cc458b324c3a9
jnarayan3039 <jp.narayan@gmail.com>
2023-02-03 11:36:28 -0600

    Update utils.py

--- codescanner/utils.py
+++ codescanner/utils.py
@@ -42,8 +42,8 @@ def get_user_with_no_id(instance_url, access_token):
     Get the Salesforce user details without knowing the user id
     """
     # Build the URL
-    url = '%s%schatter/users/me' % (instance_url, settings.SALESFORCE_REST_URL)
-
+    url = '%s%schatter/users/me' % (instance_url, REST_URL)
+    print(url)
     # Query for the user record
     result = requests.get(url, headers=get_headers(access_token))
     
